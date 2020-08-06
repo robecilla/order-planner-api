0f155ffa4251b1f0ab4706424034d441b72aeede
Roberto Vecilla <roberto.vecilla@insided.com>
2020-08-06 16:14:20 +0200

    1.0.0

--- api/app.py
+++ api/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, request
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_cors import CORS
+from db import db
+from ma import ma
+from resources.Unit import UnitListResource, UnitResource
+from resources.Recipe import RecipeListResource, RecipeResource
+from resources.Ingredient import IngredientListResource, IngredientResource
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db:3306/order_planner'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1:3307/order_planner'
 
-@app.route("/")
-def index():
-    return "Hola mundo"
+    db.init_app(app)
+    ma.init_app(app)
 
-if __name__ == "__main__":
-    app.run()
+    migrate = Migrate(app, db)
+
+    api = Api(app)
+
+    api.add_resource(UnitListResource, '/unit')
+    api.add_resource(UnitResource, '/unit/<unit_id>')
+
+    api.add_resource(RecipeListResource, '/recipe')
+    api.add_resource(RecipeResource, '/recipe/<recipe_id>')
+
+    api.add_resource(IngredientListResource, '/ingredient')
+    api.add_resource(IngredientResource, '/ingredient/<ingredient_id>')
+
+    return app
